595f1345c36a64dd740241a16485ced6df788efc
Juan Felipe Monsalve Palacio <jumonsalve@sistecredito.com>
2023-09-22 22:41:30 -0500

    Se corrige logica

--- src/main/java/com/arquitectura/reporte/Utils/Constants.java
+++ src/main/java/com/arquitectura/reporte/Utils/Constants.java
@@ -1,18 +1,13 @@
 package com.arquitectura.reporte.Utils;
 
 public class Constants {
-    public static String SQLCONSULTPARAMS = "select * from Tbl_Cultivos_Agricolas ";
+    public static String SQLCONSULTPARAMS = "select rubro, subregion, municipio, anio as año,area_total as area_Predio, area-produccion, volumen_produccion from Tbl_Cultivos_Agricolas ";
     public static String WHERE = " where ";
     public static String RUBRO = " rubro = ";
     public static String SUBREGION = " subregion = ";
     public static String ANIO = " anio = ";
     public static String AND = " and ";
     public static String SQLCONSULTIDREPORTE = "select Top 1 (id) from Tbl_solicitud_reporte order by id desc";
-
-    public static String DNSDATABASE = "jdbc:sqlserver://srv-evergreen.database.windows.net";
-    public static String USUARIO = "azureuser@srv-evergreen";
-    public static String PASS = "Q1w2e3r4123.*";
-
     public static String STRINGCONNECTION = "jdbc:sqlserver://srv-evergreen.database.windows.net:1433;database=EvergreenAna;user=azureuser@srv-evergreen;password=Q1w2e3r4123.*;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 
 }
--- src/main/java/com/arquitectura/reporte/Utils/DbConnection.java
+++ src/main/java/com/arquitectura/reporte/Utils/DbConnection.java
@@ -4,15 +4,10 @@ import java.sql.*;
 
 public class DbConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/arquitectura";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
-
     public static Connection getConnection() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(Constants.STRINGCONNECTION);
-            //connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             System.err.println("Error al conectar a la base de datos: " + e.getMessage());
         }
